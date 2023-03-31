7daa870f437330c20eb54144c3e426844b0ff8cc
Mudra9 <67046285+Mudra9@users.noreply.github.com>
2023-03-31 10:19:17 +0530

    Update check_lab.sh

--- check_lab.sh
+++ check_lab.sh
@@ -1,2 +1,2 @@
-# After running Start_io_Hub.sh, run this command to find the ip address of JupyterHub
+# After running Start_Lab.sh, run this command to find the ip address of JupyterHub
 kubectl get service --namespace jhub

6fe2f2d4388dae658cf8285ea82995a737b8bf5b
Anvit Mirjurkar <67045358+anvit121@users.noreply.github.com>
2023-05-02 01:05:45 +0530

    Update start_lab.sh

--- start_lab.sh
+++ start_lab.sh
@@ -11,7 +11,8 @@
 
 ### Download & Set Up Helm
 # https://zero-to-jupyterhub.readthedocs.io/en/latest/setup-helm.html
-curl https://raw.githubusercontent.com/helm/helm/HEAD/scripts/get-helm-3 | bash
+# curl https://raw.githubusercontent.com/helm/helm/HEAD/scripts/get-helm-3 | bash
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
 kubectl --namespace kube-system create serviceaccount tiller
 kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
 #helm init --service-account tiller
@@ -29,11 +30,11 @@ sed -i 's/<RANDOM_HEX>/'"$( openssl rand -hex 32 )"'/g' config.yaml
 helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
 helm repo update
 # Run the config.yaml file to start up JupyterHub: RELEASE=NAMESPACE=jhub, JUPYTER_VERSION=0.7.0
-# helm upgrade --cleanup-on-fail --install jhub jupyterhub/jupyterhub --namespace jhub --version=0.8.0 --values config.yaml
-helm upgrade --cleanup-on-fail \
-  --install jhub jupyterhub/jupyterhub \
-  --namespace jhub \
-  --create-namespace \
-  --version=0.8.0 \
-  --values config.yaml
+helm upgrade --cleanup-on-fail --install jhub jupyterhub/jupyterhub --namespace jhub --version=0.8.0 --values config.yaml
+# helm upgrade --cleanup-on-fail \
+#   --install jhub jupyterhub/jupyterhub \
+#   --namespace jhub \
+#   --create-namespace \
+#   --version=0.8.0 \
+#   --values config.yaml
 
